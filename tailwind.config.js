97606e4a40540354c7b459170848b27aa60e09e5
GKExpo <theshardulkamble@gmail.com>
2026-08-19 23:05:33 +0530

    chore: setup proper Tailwind CSS

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./*.tsx",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#3b82f6',
+        success: '#22c55e',
+        warning: '#f97316',
+        danger: '#ef4444',
+      }
+    },
+  },
+  plugins: [],
+}
+
